ac17b12c4116f18d269bbc92108b04bcd2404266
Hyzer <98345064+Hyzerr@users.noreply.github.com>
2022-02-22 11:25:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,11 +15,11 @@
 gc1 = ''
 gc2 = ''
 gc3 = ''
-global.linkGC = ['https://chat.whatsapp.com/HAZ6yFgCafUAeDbNH33IrL', ''] // ganti jadi group lu
-global.owner = ['+62 858‑2876‑4046'] // Put your number here //owner eval
-global.kontak = ['+62 858‑2876‑4046','0'] //Ketika ada yang ngetik #owner
-global.mods = ['+62 858‑2876‑4046'] // Want some help?
-global.prems = ['+62 858‑2876‑4046'] // Premium user has unlimited limit
+global.linkGC = ['https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV', ''] // ganti jadi group lu
+global.owner = ['6285212023766'] // Put your number here //owner eval
+global.kontak = ['6285212023766','0'] //Ketika ada yang ngetik #owner
+global.mods = ['6285212023766'] // Want some help?
+global.prems = ['6285212023766'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -63,14 +63,14 @@ namakontak2 = 'Hyzer..'
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'By Botynyakamu' // ganti aja
-global.author = 'i love u so <3' // ganti aja
+global.packname = 'By Shiro bot' // ganti aja
+global.author = 'hehe' // ganti aja
 
 //yyy
 bc = 'botynyakamu' //Bot Broadcast
-footer = '\nBotynyakamu'
-namabot = 'Botynyakamu'
-namalu = 'Botynyakamu-owner'
+footer = '\n© Shiro Botz'
+namabot = 'Shiro Botz'
+namalu = 'Hyzer愛'
 
 
 // 
